e61d008c709844745b341accdd29abc0e32de53b
Anastas Mironov <ast@l80.ru>
2022-06-23 10:04:05 +0900

    config: add custom artisan serve

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('s', function () {
+    $this->call('serve', ['--port' => '7007']);
+});
