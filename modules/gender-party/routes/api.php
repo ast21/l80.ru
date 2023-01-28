abd88d2dd16b5befe333761602327288e168a57c
Anastas Mironov <ast@l80.ru>
2023-01-28 16:57:10 +0600

    feat: add get all votes method

--- modules/gender-party/routes/api.php
+++ modules/gender-party/routes/api.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use Modules\GenderParty\Http\Controllers\VoteController;
 
+Route::get('votes', [VoteController::class, 'index']);
 Route::post('votes', [VoteController::class, 'store']);
--- modules/gender-party/src/Http/Controllers/VoteController.php
+++ modules/gender-party/src/Http/Controllers/VoteController.php
@@ -3,10 +3,17 @@
 namespace Modules\GenderParty\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Modules\GenderParty\Http\Resources\VoteResource;
 use Modules\GenderParty\Models\Vote;
 
 class VoteController extends Controller
 {
+    public function index()
+    {
+        $votes = Vote::query()->limit(4)->get();
+        return VoteResource::collection($votes);
+    }
+
     public function store(Request $request)
     {
         $validated = $request->validate([
