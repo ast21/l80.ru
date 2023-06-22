213dacc2724495b94fbaebd742f7b9a63bf785b5
Anastas Mironov <ast@l80.ru>
2023-06-23 01:35:56 +0600

    feat: add header to landing

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,7 +6,31 @@
   @vite('resources/css/app.css')
 </head>
 <body>
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<div class="container mx-auto">
+  <div class="pt-6 pb-6 flex justify-between items-center">
+    <div class="flex items-center">
+      <img src="/logo.svg" alt="logo"/>
+      <span class="ml-3 mr-4 text-2xl font-semibold">Landwind</span>
+      <ul class="flex">
+        <li class="pl-4 pr-4 hover:text-purple-700 hover:underline cursor-pointer">Company</li>
+        <li class="pl-4 pr-4 hover:text-purple-700 hover:underline cursor-pointer">Marketplace</li>
+        <li class="pl-4 pr-4 hover:text-purple-700 hover:underline cursor-pointer">Features</li>
+        <li class="pl-4 pr-4 hover:text-purple-700 hover:underline cursor-pointer">Team</li>
+        <li class="pl-4 pr-4 hover:text-purple-700 hover:underline cursor-pointer">Contact</li>
+      </ul>
+    </div>
+
+    <div class="flex">
+      <button class="bg-white hover:text-purple-700 hover:underline py-2 px-4 rounded-lg">
+        Log in
+      </button>
+      <button class="bg-purple-700 hover:bg-purple-800 text-white py-2 px-4 rounded-lg">
+        Get started
+      </button>
+    </div>
+  </div>
+</div>
+
 </body>
 @vite('resources/js/app.js')
 </html>
