92569137ad942f5b64377d922085573f5e2c43bb
Anastas Mironov <ast@l80.ru>
2023-06-23 14:20:24 +0600

    feat: add first langing page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,27 +7,60 @@
 </head>
 <body>
 <div class="container mx-auto">
-  <div class="pt-6 pb-6 flex justify-between items-center">
+  <div class="py-6 flex justify-between items-center">
     <div class="flex items-center">
       <img src="/logo.svg" alt="logo"/>
       <span class="ml-3 mr-4 text-2xl font-semibold">Landwind</span>
       <ul class="flex">
-        <li class="pl-4 pr-4 hover:text-purple-700 hover:underline cursor-pointer">Company</li>
-        <li class="pl-4 pr-4 hover:text-purple-700 hover:underline cursor-pointer">Marketplace</li>
-        <li class="pl-4 pr-4 hover:text-purple-700 hover:underline cursor-pointer">Features</li>
-        <li class="pl-4 pr-4 hover:text-purple-700 hover:underline cursor-pointer">Team</li>
-        <li class="pl-4 pr-4 hover:text-purple-700 hover:underline cursor-pointer">Contact</li>
+        <li class="px-4 hover:text-purple-700 hover:underline cursor-pointer">Company</li>
+        <li class="px-4 hover:text-purple-700 hover:underline cursor-pointer">Marketplace</li>
+        <li class="px-4 hover:text-purple-700 hover:underline cursor-pointer">Features</li>
+        <li class="px-4 hover:text-purple-700 hover:underline cursor-pointer">Team</li>
+        {{--        <li class="px-4 hover:text-purple-700 hover:underline cursor-pointer">Contact</li>--}}
       </ul>
     </div>
 
     <div class="flex">
-      <button class="bg-white hover:text-purple-700 hover:underline py-2 px-4 rounded-lg">
+      <button class="bg-white hover:text-purple-700 hover:underline py-2 px-3 rounded-lg">
         Log in
       </button>
-      <button class="bg-purple-700 hover:bg-purple-800 text-white py-2 px-4 rounded-lg">
+      <button class="bg-purple-700 hover:bg-purple-800 text-white py-2 px-3 rounded-lg">
         Get started
       </button>
     </div>
+  </div>
+
+  <div class="py-16 flex items-center">
+    <div class="w-auto">
+      <h1 class="text-6xl font-black">
+        Building digital products & brands.
+      </h1>
+      <p class="mt-6 text-xl text-gray-500">
+        Here at flowbite we focus on markets where technology, innovation,
+        and capital can unlock long-term value and drive economic growth.
+      </p>
+      <div class="flex mt-10">
+        <button class="bg-purple-700 hover:bg-purple-800 text-white py-3 px-4 rounded-lg block">
+          Start 30 day free trial
+        </button>
+        <button
+          class="ml-4 bg-white hover:bg-gray-50 hover:text-purple-800 py-3 px-4 rounded-lg border border-gray-200 block">
+          Pricing & FAQ
+        </button>
+      </div>
+
+    </div>
+    <img src="/images/marketing-strategy-1.png" alt="marketing-strategy" class="ml-3"/>
+  </div>
+
+  <div class="flex justify-between">
+    <img src="/images/google.svg" alt="google"/>
+    <img src="/images/microsoft.svg" alt="microsoft"/>
+    <img src="/images/spotify.svg" alt="spotify"/>
+    <img src="/images/mailchimp.svg" alt="mailchimp"/>
+    <img src="/images/airbnb.svg" alt="airbnb"/>
+    <img src="/images/uber.svg" alt="uber"/>
+
   </div>
 </div>
 
