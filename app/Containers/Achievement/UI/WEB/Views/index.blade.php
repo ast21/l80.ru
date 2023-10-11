ee377c248446b6680f92519153ef4e081acc660e
Anastas Mironov <ast@l80.ru>
2023-10-12 00:59:59 +0600

    feat: style cards

--- app/Containers/Achievement/UI/WEB/Views/index.blade.php
+++ app/Containers/Achievement/UI/WEB/Views/index.blade.php
@@ -6,13 +6,25 @@
   <meta title="Achievements page">
   @vite('resources/css/app.css')
 </head>
-<body>
+<body class="bg-current text-current accent-amber-50">
 
-@foreach($achievements as $achievement)
-  <img src="{{ $achievement->icon_url }}" alt="{{ $achievement->title }}">
-  <h4 class="text-2xl font-extrabold">{{ $achievement->title }}</h4>
-  <p>{{ $achievement->target }}</p>
-@endforeach
+<section class="mb-28 text-center">
+  <h3 class="my-10 text-3xl font-semibold text-gray-700 dark:text-white">
+    Achievements
+  </h3>
+
+  <div class="flex flex-wrap flex-row justify-center">
+    @foreach($achievements as $achievement)
+      <div class="block rounded-xl bg-neutral-700 w-80 m-3">
+        <img src="{{ $achievement->icon_url }}" alt="{{ $achievement->title }}"
+             class="w-20 h-20 mx-auto mt-16 rounded-xl">
+        <h4 class="mx-7 mt-10 text-amber-800 text-2xl font-extrabold text-current">{{ $achievement->title }}</h4>
+        <p class="mx-7 mt-5 pb-16">{{ $achievement->target }}</p>
+      </div>
+    @endforeach
+  </div>
+</section>
+
 
 </body>
 @vite('resources/js/app.js')
