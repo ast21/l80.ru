8c3a23eed04fb54fe8d006e114033aa7904637ae
Anastas Mironov <ast@l80.ru>
2023-12-22 00:17:39 +0900

    fix: comment domain set url config

--- app/Containers/Module/Providers/MainServiceProvider.php
+++ app/Containers/Module/Providers/MainServiceProvider.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace App\Containers\Module\Providers;
 
-use App\Containers\Module\Enum\Domain;
 use App\Ship\Abstracts\Providers\AbstractMainServiceProvider;
 
 class MainServiceProvider extends AbstractMainServiceProvider
 {
     public function boot(): void
     {
-        Domain::tryFrom(request()->getHttpHost())?->setUrlConfig();
+        //Domain::tryFrom(request()->getHttpHost())?->setUrlConfig();
         parent::boot();
     }
 }
